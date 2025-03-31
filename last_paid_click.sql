e58b30beabdedbecf9db76d267efda2c79bf0a96
viktoriyadzhoruhova <142666685+viktoriyadzhoruhova@users.noreply.github.com>
2025-03-31 13:01:41 +0500

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,41 +1,48 @@
 -- Витрина для модели атрибуции Last Paid Click
-WITH visits_ranked AS (
+WITH last_paid_click AS (
     SELECT
-        s.visitor_id,
-        s.visit_date,
-        s.source AS utm_source,
-        s.medium AS utm_medium,
-        s.campaign AS utm_campaign,
+        visitor_id,
+        visit_date,
+        source AS utm_source,
+        medium AS utm_medium,
+        campaign AS utm_campaign,
+        ROW_NUMBER() OVER (PARTITION BY visitor_id ORDER BY visit_date DESC) AS session_number
+    FROM sessions
+    WHERE
+        medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+),
+latest_paid_click AS (
+    SELECT
+        visitor_id,
+        visit_date,
+        utm_source,
+        utm_medium,
+        utm_campaign
+    FROM last_paid_click
+    WHERE session_number = 1
+),
+attributed_leads AS (
+    SELECT
+        lpc.visitor_id,
+        lpc.visit_date,
+        lpc.utm_source,
+        lpc.utm_medium,
+        lpc.utm_campaign,
         l.lead_id,
         l.created_at,
         l.amount,
         l.closing_reason,
-        l.status_id,
-        ROW_NUMBER() OVER (PARTITION BY s.visitor_id ORDER BY s.visit_date DESC) AS rn
-    FROM sessions AS s
+        l.status_id
+    FROM
+        latest_paid_click AS lpc
     LEFT JOIN leads AS l
-        ON s.visitor_id = l.visitor_id
-        AND l.created_at >= s.visit_date
-    WHERE s.medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+        ON lpc.visitor_id = l.visitor_id AND lpc.visit_date <= l.created_at
 )
-SELECT
-    visitor_id,
-    visit_date,
-    utm_source,
-    utm_medium,
-    utm_campaign,
-    lead_id,
-    created_at,
-    amount,
-    closing_reason,
-    status_id
-FROM visits_ranked
-WHERE rn = 1
+SELECT *
+FROM attributed_leads
 ORDER BY
+    amount DESC NULLS LAST,
     visit_date ASC,
     utm_source ASC,
     utm_medium ASC,
-    utm_campaign ASC,
-    lead_id ASC;
-
-
+    utm_campaign ASC;
